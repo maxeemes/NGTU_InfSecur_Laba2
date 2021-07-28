eb773485f56a94c2695a2a0b3dba1a976b0024b3
maxeemes <max.s.99@yandex.ru>
2021-07-29 01:32:09 +0300

    start

---
+++ NGTU_InfSecur_Laba2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+bool AddConsoleTextColor(const string Text, const int Color = 7);//÷вета: 7 - белый(стандартный), 10 - зеленый, 12 - красный, 14 - желтый, 224 - черный на желтом фоне
+int ReadConsoleNum(const string Text = "", const int Color = 7);
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+	return 0;
+}
+
+bool AddConsoleTextColor(const string Text, const int Color)//÷вета: 7 - белый(стандартный), 10 - зеленый, 12 - красный, 14 - желтый, 224 - черный на желтом фоне
+{
+	if (Color == 7) {
+		cout << Text << endl;
+	}
+	else {
+		HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(Console, Color);
+		cout << Text << endl;
+		SetConsoleTextAttribute(Console, 7);
+	}
+	return true;
+}
+
+int ReadConsoleNum(const string Text, const int Color)
+{
+	if (!Text.empty())
+	{
+		AddConsoleTextColor(Text, Color);
+	}
+	int Num;
+	cin >> Num;
+	if (Num >= 0)
+	{
+		return Num;
+	}
+	return -1;
+}
